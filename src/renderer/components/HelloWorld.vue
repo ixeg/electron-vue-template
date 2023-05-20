0fc3da1c7dcb58228afe4327cdec05b65f2d0961
Juan Ruiz <juanruiz425@gmail.com>
2023-05-20 16:45:47 -0500

    Set up for pdf to excel
    Electron app that will use an OCR into a an excel file. Will have a UI to help organize text quickly for service reports

--- src/renderer/components/HelloWorld.vue
+++ src/renderer/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -18,11 +18,6 @@ const count = ref(0)
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
